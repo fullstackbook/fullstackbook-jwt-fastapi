592b657fd21dc00aac99c69bb3f5d2dcfc57d1bb
Travis Luong <travis@travisluong.com>
2022-09-01 23:38:30 -0700

    conform to api contract

--- routers/content.py
+++ routers/content.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends
+from dependencies.auth import get_current_active_user
 
 from dependencies.roles import admin_role, moderator_role, user_role
+from model import User
+from schemas import UserRead
 
-router = APIRouter()
+router = APIRouter(prefix="/api/test")
 
 @router.get("/admin")
 def admin(current_user = Depends(admin_role)):
@@ -12,10 +15,15 @@ def admin(current_user = Depends(admin_role)):
 def user(current_user = Depends(user_role)):
     return "user content"
 
-@router.get("/moderator")
+@router.get("/mod")
 def moderator(current_user = Depends(moderator_role)):
     return "moderator content"
 
 @router.get("/all")
 def all():
     return "public content"
+
+@router.get("/profile", response_model=UserRead)
+def read_users_me(current_user: User = Depends(get_current_active_user)):
+    user = UserRead(username=current_user.username, email=current_user.email)
+    return user
--- schemas.py
+++ schemas.py
@@ -1,8 +1,12 @@
 from typing import Optional
 from pydantic import BaseModel
 
+class SignInRequest(BaseModel):
+    username: str
+    password: str
+
 class Token(BaseModel):
-    access_token: str
+    token: str
     token_type: str
 
 class TokenData(BaseModel):
